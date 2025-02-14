d1335c0f7f72f4d0bd69f13660b9928f5a3a2a91
Ouail Amarir <ouailamarir@gmail.com>
2025-02-14 12:01:06 +0100

    Mise à jour du titre de la page.

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= isset($title) ? $title : "Photo Management" ?></title>
+    <title><?= isset($title) ? $title : "RoadPic - Capturez l’Aventure, Partagez l’Instant" ?></title>
     <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
