364a8f98050d8506fce9e02983efd4afab7383db
Yanis Pires <ypires@fiveforty.fr>
2025-02-17 16:49:16 +0100

    Correction de commentaires et ajout de Composer
    Modifications dans `Controller.php` pour corriger des caractères spéciaux et ajuster les chemins des fichiers de modèle et de vue. Suppression du chargement des fichiers d'en-tête et de pied de page pour simplifier la méthode `view`.
    
    Ajout d'instructions dans le `Dockerfile` pour installer Composer, incluant le téléchargement et l'exécution du script d'installation. La commande par défaut a été mise à jour en conséquence.
    
    Un fichier binaire `67b344007f46a.png` a été ajouté.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,10 +2,10 @@
 // app/core/Controller.php
 class Controller {
     public function model($model) {
-        // Chemin complet vers le modèle
+        // Chemin complet vers le mod�le
         $modelFile = ROOT . '/models/' . $model . '.php';
         
-        // Vérifier si le fichier existe
+        // V�rifier si le fichier existe
         if (file_exists($modelFile)) {
             require_once $modelFile;
             return new $model();
@@ -15,29 +15,17 @@ class Controller {
 
     public function view($view, $data = []) {
         // Chemin complet vers la vue
-        $viewFile = ROOT . '/Views/' . $view . '.php';
-        $headerFile = ROOT . '/Views/layouts/header.php';
-        $footerFile = ROOT . '/Views/layouts/footer.php';
+        $viewFile = ROOT . '/views/' . $view . '.php';
         
-        // Vérifier si la vue existe
+        // V�rifier si la vue existe
         if (!file_exists($viewFile)) {
             die("View not found: " . $viewFile);
         }
 
-        // Extraire les données pour les rendre disponibles dans la vue
+        // Extraire les donn�es pour les rendre disponibles dans la vue
         extract($data);
-
-        // Charger le header s'il existe
-        if (file_exists($headerFile)) {
-            require $headerFile;
-        }
 
         // Charger la vue principale
         require $viewFile;
-
-        // Charger le footer s'il existe
-        if (file_exists($footerFile)) {
-            require $footerFile;
-        }
     }
 }
